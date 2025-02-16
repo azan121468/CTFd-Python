349a43a1fb196943685df4e2f5a61fa4f7acea0f
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2025-02-16 22:00:34 +0500

    Added script to fix path when working on both windows and linux

---
+++ fix_path.py
@@ -0,0 +1,46 @@
+import os
+import re
+
+#http://geeksforgeeks.org/python-list-all-files-in-directory-and-subdirectories/
+def get_all_automation_files(directory):
+    automation_files = []
+    
+    for root, dirs, files in os.walk(directory):
+        # print(f"{root=} {dirs=} {files=}")
+        # breakpoint()
+        if root == '.': # skip the current directory because this contain base automation files and not helper files
+            continue
+        if re.match(r'^.*helper_[0-9a-f]{6}$', root):
+            # print(f"{root=}")
+            for file in files:
+                automation_files.append(os.path.join(root, file))
+    
+    return automation_files
+
+def fix_config_path(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    
+    config_line_index = 0
+    for i in range(len(lines)):
+        if 'config_file = ' in lines[i]:
+            config_line_index = i
+            break
+    # print(config_line_index)
+
+    #Credits: ChatGPT for next three lines
+    current_path = os.path.normpath(os.getcwd())  # Converts to the OS-specific format
+    current_path = current_path.replace("\\", "/")  # Use forward slashes for cross-platform compatibility
+    line = re.sub(r'(\.join\(r\")(.*?)(\", )', rf'\1{current_path}\3', lines[config_line_index])
+    
+    lines[config_line_index] = line
+
+    with open(file, 'w') as f:
+        f.writelines(lines)
+
+
+
+files = get_all_automation_files('.')
+
+for file in files:
+    fix_config_path(file)
